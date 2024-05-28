11737dab45941daee28777565440c69383d98fa9
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-05-28 14:59:31 +1000

    asdfasiudfhiashduoif

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 from shapes import Shape , Rect , Circle
 
-bungle = Rect(2 , 5)
+while True:
+    print("SHAPE CALCULATOR")
+    userChoice = input("press R for rectangle or C for circle       ")
+        
+    if userChoice.upper() == "R":
+        print("rectangle selected")
+        W = input("enter width")
+        H = input("enter height")
+
+        
+        
+        userRect = Rect( W , H)
+        
+        print("perimeter = " , userRect.perimeter())
+        print("area = " , userRect.area())
+
+    elif userChoice.upper() == "C":
+        print("circle selected")
+        R = input("enter radius")
+        
+        userCircle = Circle(R)
+        print(userCircle.perimeter())
+        print(userCircle.area())
+
+    else:
+        print("invalid input")
+        continue
+        
+
+
